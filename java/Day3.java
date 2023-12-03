9aca600db99c65e5725790728fb15f6c36ebb793
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-02 22:37:56 -0800

    d3

--- java/Day3.java
+++ java/Day3.java
@@ -1,128 +1,87 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day3 {
+    static char[][] grid;
+    static int row;
+    static int col;
+    static final int[][] DIRECT = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+    
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.currentTimeMillis();
-        
         Scanner in = new Scanner(new FileReader("input/day3.txt"));
         List<String> input = new ArrayList<>();
-        while (in.hasNextLine()) input.add(in.nextLine());
+        while (in.hasNextLine()) {
+            input.add(in.nextLine());
+        }
         
-        int total = part11(input);
-        int remove = part1(input);
-        System.out.println("total: " + total);
-        System.out.println("removing: " + remove);
-        System.out.println("Part I : " + (total - remove));
-        //        System.out.println("Part II: " + part2(input));
-        //        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("Part I  514969  : " + sumPartNumber(input));
+        System.out.println("Part II 78915902: " + sumGearRatio(input));
+        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
-    private static int part11(List<String> input) {
-        int row = input.size();
-        int col = input.get(0).length();
-        char[][] grid = new char[row][input.get(0).length()];
-        for (int r = 0; r < row; r++) {
-            grid[r] = input.get(r).toCharArray();
-        }
-        for (int r = 0; r < row; r++) {
-            for (int c = 0; c < col; c++) {
-                if (!Character.isDigit(grid[r][c]) && grid[r][c] != '.') {
-                    grid[r][c] = '.';
-                }
-            }
-        }
-        
-        Pattern pattern = Pattern.compile("\\d+");
-        int total = 0;
-        for (char[] chars : grid) {
-            String line = new String(chars);
-            Matcher matcher = pattern.matcher(line);
-            while (matcher.find()) {
-                int newNum = Integer.parseInt(matcher.group());
-                //                System.out.println(newNum);
-                total += newNum;
-            }
-        }
-        return total;
-    }
-    
-    private static int part1(List<String> input) {
-        int row = input.size();
-        int col = input.get(0).length();
-        char[][] grid = new char[row][input.get(0).length()];
-        for (int r = 0; r < row; r++) {
-            grid[r] = input.get(r).toCharArray();
-        }
-        
-        int[][] direct = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-        for (int r = 0; r < row; r++) {
-            for (int c = 0; c < col; c++) {
-                //  && grid[r][c] != 'T'
-                if (!Character.isDigit(grid[r][c]) && grid[r][c] != '.') {
-                    //                    System.out.println(r + ", " + c + ": " + grid[r][c]);
-                    for (int[] dr : direct) {
-                        int rr = r + dr[0];
-                        int cc = c + dr[1];
-                        if (rr >= 0 && rr < row && cc >= 0 && cc < col && Character.isDigit(grid[rr][cc])) {
-                            grid[rr][cc] = 'T';
-                        }
-                    }
-                    grid[r][c] = 'T';
-                }
-            }
-        }
-        
+    private static int sumPartNumber(List<String> input) {
+        getGrid(input);
         int res = 0;
         for (int r = 0; r < row; r++) {
             for (int c = 0; c < col; c++) {
-                if (grid[r][c] == 'T') {
-                    int idx = c + 1;
-                    while (idx < col && Character.isDigit(grid[r][idx])) {
-                        grid[r][idx] = 'T';
-                        idx++;
-                    }
-                    idx = c - 1;
-                    while (idx >= 0 && Character.isDigit(grid[r][idx])) {
-                        grid[r][idx] = 'T';
-                        idx--;
-                    }
-                }
-                if (Character.isDigit(grid[r][c])) {
-                    StringBuilder part = new StringBuilder();
-                    int idx = c;
-                    while (idx < col) {
-                        if (grid[r][idx] == '.') {
-                            c = idx;
-                            System.out.println(Integer.parseInt(part.toString()));
-                            res += Integer.parseInt(part.toString());
-                            break;
-                        } else if (grid[r][idx] == 'T') {
-                            break;
-                        } else {
-                            part.append(grid[r][idx]);
-                            //                            grid[r][idx] = 'N';
-                            idx++;
+                if (!Character.isDigit(grid[r][c]) && grid[r][c] != '.') {
+                    for (int[] dr : DIRECT) {
+                        int rr = r + dr[0];
+                        int cc = c + dr[1];
+                        if (rr >= 0 && rr < row && cc >= 0 && cc < col && Character.isDigit(grid[rr][cc])) {
+                            res += getSurroundingNum(rr, cc);
                         }
                     }
                 }
             }
         }
-        
-        printer(grid);
-        
-        return res; // not 515900, 508206
+        return res;
     }
     
-    private static void printer(char[][] grid) {
-        for (char[] chars : grid) {
-            System.out.println(Arrays.toString(chars));
+    private static int sumGearRatio(List<String> input) {
+        getGrid(input);
+        int res = 0;
+        List<Integer> currList = new ArrayList<>();
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == '*') {
+                    for (int[] dr : DIRECT) {
+                        int rr = r + dr[0];
+                        int cc = c + dr[1];
+                        if (rr >= 0 && rr < row && cc >= 0 && cc < col && Character.isDigit(grid[rr][cc])) {
+                            currList.add(getSurroundingNum(rr, cc));
+                        }
+                    }
+                    if (currList.size() == 2) {
+                        res += currList.get(0) * currList.get(1);
+                    }
+                    currList = new ArrayList<>();
+                }
+            }
         }
+        return res;
     }
     
-    private static int part2(List<String> input) {
-        return -1;
+    private static int getSurroundingNum(int r, int c) {
+        while (c - 1 >= 0 && Character.isDigit(grid[r][c - 1])) {
+            c--;
+        }
+        StringBuilder res = new StringBuilder();
+        while (c < col && Character.isDigit(grid[r][c])) {
+            res.append(grid[r][c]);
+            grid[r][c] = 'N';
+            c++;
+        }
+        return Integer.parseInt(res.toString());
+    }
+    
+    private static void getGrid(List<String> input) {
+        row = input.size();
+        col = input.get(0).length();
+        grid = new char[row][input.get(0).length()];
+        for (int r = 0; r < row; r++) {
+            grid[r] = input.get(r).toCharArray();
+        }
     }
 }

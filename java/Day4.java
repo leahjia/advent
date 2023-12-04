942ec751f8aa385ddbe1f535be86fd331fed3b08
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-03 22:28:00 -0800

    .

--- java/Day4.java
+++ java/Day4.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Day4 {
-    static HashSet<Integer>[] winning;
-    static int[][] having;
+    static HashSet<Integer>[] winningCards;
+    static int[][] cardsInHand;
     static int nCards;
     
     public static void main(String[] args) throws FileNotFoundException {
@@ -14,17 +14,17 @@ public class Day4 {
             input.add(in.nextLine());
         }
         getCards(input);
-        System.out.println("Part I  21821  : " + part1());
-        System.out.println("Part II 5539496: " + part2());
+        System.out.println("Part I  21821  : " + getPoints());
+        System.out.println("Part II 5539496: " + getCopies());
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
-    private static int part1() {
+    private static int getPoints() {
         int res = 0;
         for (int i = 0; i < nCards; i++) {
             int ct = 0;
-            for (int card : having[i]) {
-                if (winning[i].contains(card)) {
+            for (int card : cardsInHand[i]) {
+                if (winningCards[i].contains(card)) {
                     ct *= 2;
                     if (ct == 0) ct = 1;
                 }
@@ -34,11 +34,11 @@ public class Day4 {
         return res;
     }
     
-    private static int part2() {
+    private static int getCopies() {
         Map<Integer, Integer> matches = new HashMap<>();
         for (int i = 0; i < nCards; i++) {
-            for (int card : having[i]) {
-                if (winning[i].contains(card)) {
+            for (int card : cardsInHand[i]) {
+                if (winningCards[i].contains(card)) {
                     matches.put(i + 1, matches.getOrDefault(i + 1, 0) + 1);
                 }
             }
@@ -63,22 +63,23 @@ public class Day4 {
     
     private static void getCards(List<String> input) {
         nCards = input.size();
-        winning = new HashSet[nCards];
-        having = new int[nCards][];
+        winningCards = new HashSet[nCards];
+        cardsInHand = new int[nCards][];
         for (int card = 0; card < nCards; card++) {
             String line = input.get(card);
-            String[] winAndHave = line.split(": ")[1].split(" \\| ");
-            winning[card] = new HashSet<>();
-            for (String s : winAndHave[0].split(" ")) {
-                if (s.equals("")) continue;
-                winning[card].add(Integer.parseInt(s));
+            String[] allCards = line.split(": ")[1].split(" \\| ");
+            winningCards[card] = new HashSet<>();
+            
+            String[] currList = allCards[0].trim().split("\\s+");
+            for (String num : currList) {
+                winningCards[card].add(Integer.parseInt(num));
             }
-            List<Integer> currList = new ArrayList<>();
-            for (String s : winAndHave[1].split(" ")) {
-                if (s.equals("")) continue;
-                currList.add(Integer.parseInt(s));
+            
+            currList = allCards[1].trim().split("\\s+");
+            cardsInHand[card] = new int[currList.length];
+            for (int i = 0; i < currList.length; i++) {
+                cardsInHand[card][i] = Integer.parseInt(currList[i]);
             }
-            having[card] = currList.stream().mapToInt(i -> i).toArray();
         }
     }
 }

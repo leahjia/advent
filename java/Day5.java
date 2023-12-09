bc7643665cea6e2fac3bebc82274cfd90cddb042
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-08 16:07:56 -0800

    slight opt

--- java/Day5.java
+++ java/Day5.java
@@ -2,94 +2,78 @@ import java.io.*;
 import java.util.*;
 
 public class Day5 {
-    static String[] mapNames = new String[]{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"};
+    static String[] mapNames;
     static long[] seeds;
-    static Map<String, Map<long[], Long>> maps; // <name, <range, diff>>
+    static Map<String, List<long[]>> maps; // <name, <range, diff>>
     
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.currentTimeMillis();
-        Scanner in = new Scanner(new FileReader("input/day5.txt"));
-        processInput(in);
-        
-        System.out.println("Part I  424490994 : " + part1());
-        System.out.println("Part II  15290096 : " + part2());
+        processInput(new Scanner(new FileReader("input/day5.txt")));
+        System.out.println("Part I  424490994 : " + getLowestSeed());
+        System.out.println("Part II  15290096 : " + getLowestInRange());
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
-    private static long part1() {
+    private static long getLowestSeed() {
         long res = Long.MAX_VALUE;
         for (long seed : seeds) {
-            long next = seed;
             for (String mapName : mapNames) {
-                next = getMapTo(maps.get(mapName), next);
+                seed = getMapTo(maps.get(mapName), seed);
             }
-            res = Math.min(res, next);
+            res = Math.min(res, seed);
         }
         return res;
     }
     
-    private static long getMapTo(Map<long[], Long> map, long n) {
-        for (Map.Entry<long[], Long> e : map.entrySet()) {
-            long[] range = e.getKey();
+    private static long getMapTo(List<long[]> ranges, long n) {
+        for (long[] range : ranges) {
             if (range[0] <= n && n <= range[1]) {
-                return n + e.getValue();
+                return n + range[2];
             }
         }
         return n;
     }
     
-    private static long part2() {
-        long res = Long.MAX_VALUE;
+    private static long getLowestInRange() {
         List<long[]> mapped = new ArrayList<>();
         for (int i = 0; i < seeds.length - 1; i += 2) {
             mapped.add(new long[]{seeds[i], seeds[i] + seeds[i + 1] - 1});
         }
         for (String mapName : mapNames) {
-            List<long[]> newMapped = getMapToRange(maps.get(mapName), mapped);
-            mapped = new ArrayList<>(newMapped);
+            mapped = getMapToRange(maps.get(mapName), mapped);
         }
+        long lowest = Long.MAX_VALUE;
         for (long[] range : mapped) {
-            res = Math.min(res, range[0]);
+            lowest = Math.min(lowest, range[0]);
         }
-        return res;
+        return lowest;
     }
     
-    private static List<long[]> getMapToRange(Map<long[], Long> map, List<long[]> ranges) {
+    private static List<long[]> getMapToRange(List<long[]> map, List<long[]> ranges) {
         List<long[]> mapped = new ArrayList<>();
         for (long[] range : ranges) {
-            long x1 = range[0];
-            long y1 = range[1];
-            for (Map.Entry<long[], Long> e : map.entrySet()) {
-                long[] mapRange = e.getKey();
-                long x0 = mapRange[0];
-                long y0 = mapRange[1];
-                long diff = e.getValue();
+            for (long[] mapRange : map) {
+                long offBy = mapRange[2];
                 if (hasOverlap(range, mapRange)) {
-                    long localMin = Math.max(x0, x1) + diff;
-                    long localMax = Math.min(y0, y1) + diff;
-                    mapped.add(new long[]{localMin, localMax});
+                    mapped.add(new long[]{Math.max(mapRange[0], range[0]) + offBy, Math.min(mapRange[1], range[1]) + offBy});
                 }
             }
         }
         return mapped;
     }
     
-    private static boolean hasOverlap(long[] a, long[] b) {
-        return b[0] <= a[0] && a[0] <= b[1] || b[0] <= a[1] && a[1] <= b[1] ||
-                a[0] <= b[0] && b[0] <= a[1] || a[0] <= b[1] && b[1] <= a[1];
+    private static boolean hasOverlap(long[] range1, long[] range2) {
+        return !(range1[1] < range2[0] || range2[1] < range1[0]);
     }
     
     private static void processInput(Scanner in) {
-        String[] temp = in.nextLine().split(": ")[1].split(" ");
-        seeds = new long[temp.length];
-        for (int i = 0; i < temp.length; i++) {
-            seeds[i] = Long.parseLong(temp[i]);
-        }
+        mapNames = new String[]{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"};
+        seeds = Arrays.stream(in.nextLine().split(": ")[1].split(" ")).mapToLong(Long::parseLong).toArray();
         
         // <range, diff>
         maps = new HashMap<>();
         for (String key : mapNames) {
-            maps.put(key, new HashMap<>());
+            maps.put(key, new ArrayList<>());
         }
         in.nextLine();
         
@@ -97,14 +81,14 @@ public class Day5 {
         while (in.hasNextLine()) {
             in.nextLine();
             String key = mapNames[mapId];
-            Map<long[], Long> currMap = maps.get(key);
+            List<long[]> currMap = maps.get(key);
             
             long lowEnd = Long.MAX_VALUE;
             long highEnd = Long.MIN_VALUE;
             
             while (in.hasNextLine()) {
                 String line = in.nextLine();
-                if (line.length() == 0) {
+                if (line.isEmpty()) {
                     mapId++;
                     break;
                 }
@@ -112,14 +96,14 @@ public class Day5 {
                 long src = Long.parseLong(entry[1]);
                 long end = src + Long.parseLong(entry[2]) - 1;
                 long offBy = Long.parseLong(entry[0]) - src;
-                currMap.put(new long[]{src, end}, offBy); // stupid off by 1
+                currMap.add(new long[]{src, end, offBy}); // stupid off by 1
                 
                 lowEnd = Math.min(lowEnd, src - 1);
                 highEnd = Math.max(highEnd, end + 1);
             }
             // just trying to cover all ranges
-            currMap.put(new long[]{Long.MIN_VALUE, lowEnd}, (long) 0);
-            currMap.put(new long[]{highEnd, Long.MAX_VALUE}, (long) 0);
+            currMap.add(new long[]{Long.MIN_VALUE, lowEnd, (long) 0});
+            currMap.add(new long[]{highEnd, Long.MAX_VALUE, (long) 0});
         }
     }
 }

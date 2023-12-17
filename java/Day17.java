1cae252ecef481db9050ef00d5083ba68cf5646b
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-17 01:50:05 -0800

    dij

--- java/Day17.java
+++ java/Day17.java
@@ -1,13 +1,27 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.Pattern;
+
+class Cell {
+    int r;
+    int c;
+    long heat;
+    int[] dir;
+    int sameDir;
+    
+    Cell(int r, int c, long heat, int[] dir, int sameDir) {
+        this.r = r;
+        this.c = c;
+        this.heat = heat;
+        this.dir = dir;
+        this.sameDir = sameDir;
+    }
+}
 
 public class Day17 {
     static int[] up = {-1, 0};
     static int[] down = {1, 0};
     static int[] left = {0, -1};
     static int[] right = {0, 1};
-    static int[][] direct = {up, down, left, right};
     static int[][] grid;
     static int row;
     static int col;
@@ -16,61 +30,75 @@ public class Day17 {
         long startTime = System.currentTimeMillis();
         Scanner in = new Scanner(new FileReader("input/day17_sample.txt"));
         processInput(in);
-        System.out.println("Part I  ?? : " + part1());
+        System.out.println("Part I  [, 953] : " + part1());
         System.out.println("Part II ?? : " + part2());
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
-    static long minHeat;
-    static long[][][] cache;
-    
     private static long part1() {
-        minHeat = Long.MAX_VALUE;
-        cache = new long[row][col][4];
+        PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a.heat));
+        long[][][] visitedHeat = new long[row][col][4];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                Arrays.fill(cache[i][j], Long.MAX_VALUE);
+                Arrays.fill(visitedHeat[i][j], Long.MAX_VALUE);
             }
         }
-        //        backtrack(0, 1, new boolean[row][col], 1, new int[]{0, 1}, 0);
-        backtrack(1, 0, new boolean[row][col], 1, new int[]{1, 0}, 0);
-        return minHeat;
-    }
-    
-    private static void backtrack(int r, int c, boolean[][] visit, int sameDir, int[] currDir, long currHeat) {
-        if (r < 0 || c < 0 || r >= row || c >= col || visit[r][c] || sameDir > 3 || currHeat >= minHeat || cache[r][c][sameDir] <= currHeat) {
-            return;
-        }
-        visit[r][c] = true;
-        currHeat += grid[r][c];
-        cache[r][c][sameDir] = currHeat; // we know it's lower otherwise would've returned early
         
-        if (r == row - 1 && c == col - 1) {
-            // System.out.println(currHeat + " by going " + path);
-            minHeat = Math.min(minHeat, currHeat);
-        } else {
-            for (int[] dr : getDirect(currDir)) {
-                int newCount = Arrays.equals(dr, currDir) ? (sameDir + 1) : 1;
-                backtrack(r + dr[0], c + dr[1], visit, newCount, dr, currHeat);
+        boolean[][][] visited = new boolean[row][col][4];
+        Cell start = new Cell(0, 0, 0, new int[]{1, 0}, 0);
+        pq.offer(start);
+        visitedHeat[0][0][0] = 0;
+        
+        while (!pq.isEmpty()) {
+            Cell curr = pq.poll();
+            int r = curr.r;
+            int c = curr.c;
+            
+            if (r == row - 1 && c == col - 1) {
+                return curr.heat;
+            }
+            
+            if (visited[r][c][curr.sameDir]) {
+                continue;
+            }
+            visited[r][c][curr.sameDir] = true;
+            
+            for (int[] dr : getDirect(curr.dir)) {
+                int rr = r + dr[0];
+                int cc = c + dr[1];
+                if (rr < 0 || cc < 0 || rr >= row || cc >= col) continue;
+                
+                int newDir = isSameDir(curr.dir, dr) ? (curr.sameDir + 1) : 1;
+                if (newDir > 3) continue;
+                
+                long newHeat = curr.heat + grid[rr][cc];
+                Cell newCell = new Cell(rr, cc, newHeat, dr, newDir);
+                
+                pq.offer(newCell);
             }
         }
-        visit[r][c] = false;
+        
+        return Long.MAX_VALUE;
     }
     
     private static int[][] getDirect(int[] currDir) {
-        if (Arrays.equals(currDir, up)) {
+        if (isSameDir(currDir, up)) {
             return new int[][]{up, left, right};
         }
-        if (Arrays.equals(currDir, down)) {
+        if (isSameDir(currDir, down)) {
             return new int[][]{down, left, right};
         }
-        if (Arrays.equals(currDir, left)) {
+        if (isSameDir(currDir, left)) {
             return new int[][]{up, down, left};
         }
-        if (Arrays.equals(currDir, right)) {
+        if (isSameDir(currDir, right)) {
             return new int[][]{up, down, right};
         }
         return new int[0][];
+    }
+    
+    private static boolean isSameDir(int[] a, int[] b) {
+        return a[0] == b[0] && a[1] == b[1];
     }
     
     private static long part2() {

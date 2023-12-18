94ec10030994d3e248d028abefa346e049d59879
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-17 16:06:08 -0800

    bruh

--- java/Day17.java
+++ java/Day17.java
@@ -1,109 +1,98 @@
 import java.io.*;
 import java.util.*;
 
-class Cell {
-    int r;
-    int c;
-    long heat;
-    int[] dir;
-    int sameDir;
-    
-    Cell(int r, int c, long heat, int[] dir, int sameDir) {
-        this.r = r;
-        this.c = c;
-        this.heat = heat;
-        this.dir = dir;
-        this.sameDir = sameDir;
-    }
-}
-
 public class Day17 {
-    static int[] up = {-1, 0};
-    static int[] down = {1, 0};
-    static int[] left = {0, -1};
-    static int[] right = {0, 1};
+    static int[][] direct = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int[][] grid;
     static int row;
     static int col;
     
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.currentTimeMillis();
-        Scanner in = new Scanner(new FileReader("input/day17_sample.txt"));
+        Scanner in = new Scanner(new FileReader("input/day17.txt"));
         processInput(in);
-        System.out.println("Part I  [, 953] : " + part1());
-        System.out.println("Part II ?? : " + part2());
+        System.out.println("Part I   942 : " + part1());
+        System.out.println("Part II 1082 : " + part2());
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
     private static long part1() {
         PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a.heat));
-        long[][][] visitedHeat = new long[row][col][4];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                Arrays.fill(visitedHeat[i][j], Long.MAX_VALUE);
-            }
-        }
-        
-        boolean[][][] visited = new boolean[row][col][4];
-        Cell start = new Cell(0, 0, 0, new int[]{1, 0}, 0);
+        Set<String> visit = new HashSet<>(); // stupid visit set took me forever
+        Cell start = new Cell(0, 0, 0, 0, 0, 0);
         pq.offer(start);
-        visitedHeat[0][0][0] = 0;
         
         while (!pq.isEmpty()) {
             Cell curr = pq.poll();
-            int r = curr.r;
-            int c = curr.c;
             
-            if (r == row - 1 && c == col - 1) {
+            if (curr.r == row - 1 && curr.c == col - 1) {
                 return curr.heat;
             }
             
-            if (visited[r][c][curr.sameDir]) {
+            String key = curr.r + "," + curr.c + "," + curr.dr + "," + curr.dc + "," + curr.n;
+            if (visit.contains(key)) {
                 continue;
             }
-            visited[r][c][curr.sameDir] = true;
+            visit.add(key);
             
-            for (int[] dr : getDirect(curr.dir)) {
-                int rr = r + dr[0];
-                int cc = c + dr[1];
-                if (rr < 0 || cc < 0 || rr >= row || cc >= col) continue;
+            for (int[] dr : direct) {
+                int rr = curr.r + dr[0];
+                int cc = curr.c + dr[1];
+                if (rr < 0 || cc < 0 || rr >= row || cc >= col || curr.dr == -dr[0] && curr.dc == -dr[1]) {
+                    continue;
+                }
                 
-                int newDir = isSameDir(curr.dir, dr) ? (curr.sameDir + 1) : 1;
-                if (newDir > 3) continue;
+                int newDir = 1;
+                if (curr.dr == dr[0] && curr.dc == dr[1]) {
+                    newDir += curr.n;
+                }
+                if (newDir > 3) {
+                    continue;
+                }
                 
-                long newHeat = curr.heat + grid[rr][cc];
-                Cell newCell = new Cell(rr, cc, newHeat, dr, newDir);
-                
-                pq.offer(newCell);
+                pq.offer(new Cell(rr, cc, curr.heat + grid[rr][cc], dr[0], dr[1], newDir));
             }
         }
         
         return Long.MAX_VALUE;
     }
     
-    private static int[][] getDirect(int[] currDir) {
-        if (isSameDir(currDir, up)) {
-            return new int[][]{up, left, right};
-        }
-        if (isSameDir(currDir, down)) {
-            return new int[][]{down, left, right};
-        }
-        if (isSameDir(currDir, left)) {
-            return new int[][]{up, down, left};
-        }
-        if (isSameDir(currDir, right)) {
-            return new int[][]{up, down, right};
-        }
-        return new int[0][];
-    }
-    
-    private static boolean isSameDir(int[] a, int[] b) {
-        return a[0] == b[0] && a[1] == b[1];
-    }
-    
     private static long part2() {
-        long res = 0;
-        return res;
+        PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a.heat));
+        Set<String> visit = new HashSet<>();
+        Cell start = new Cell(0, 0, 0, 0, 0, 0);
+        pq.offer(start);
+        
+        while (!pq.isEmpty()) {
+            Cell curr = pq.poll();
+            
+            if (curr.r == row - 1 && curr.c == col - 1 && curr.n >= 4) {
+                return curr.heat;
+            }
+            
+            String key = curr.r + "," + curr.c + "," + curr.dr + "," + curr.dc + "," + curr.n;
+            if (visit.contains(key)) {
+                continue;
+            }
+            visit.add(key);
+            
+            for (int[] dr : direct) {
+                int rr = curr.r + dr[0];
+                int cc = curr.c + dr[1];
+                if (rr < 0 || cc < 0 || rr >= row || cc >= col || curr.dr == -dr[0] && curr.dc == -dr[1]) {
+                    continue;
+                }
+                
+                if (curr.dr == dr[0] && curr.dc == dr[1]) {
+                    if (curr.n < 10) {
+                        pq.offer(new Cell(rr, cc, curr.heat + grid[rr][cc], dr[0], dr[1], curr.n + 1));
+                    }
+                } else if (curr.n >= 4 || curr.n == 0) {
+                    pq.offer(new Cell(rr, cc, curr.heat + grid[rr][cc], dr[0], dr[1], 1));
+                }
+            }
+        }
+        return Long.MAX_VALUE;
     }
     
     private static void processInput(Scanner in) {
@@ -120,5 +109,21 @@ public class Day17 {
                 grid[r][c] = Character.getNumericValue(input.get(r)[c]);
             }
         }
+    }
+}
+
+class Cell {
+    int r, c;
+    long heat;
+    int dr, dc;
+    int n;
+    
+    Cell(int r, int c, long heat, int dr, int dc, int n) {
+        this.r = r;
+        this.c = c;
+        this.heat = heat;
+        this.dr = dr;
+        this.dc = dc;
+        this.n = n;
     }
 }

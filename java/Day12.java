b10f17fbecbd81cde1073589ef2c3b7207af25d9
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-19 14:41:45 -0800

    d12

--- java/Day12.java
+++ java/Day12.java
@@ -1,109 +1,107 @@
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Day12 {
-    static List<char[]> puzzle;
-    static List<int[]> condition;
-    static int len;
-    
-    public static void main(String[] args) throws FileNotFoundException {
-        long startTime = System.currentTimeMillis();
-        Scanner in = new Scanner(new FileReader("input/day12.txt"));
-        processInput(in);
-        // System.out.println("Part I  6958 : " + countArrange(1));
-        System.out.println("Part II ?? : " + countArrange(5));
-        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
-    }
-    
+    static List<String> puzzles;
+    static List<List<Integer>> conditions;
     static long count = 0;
     
-    private static long countArrange(int wrap) {
+    public static void main(String[] args) throws IOException {
+        List<String> input = Files.readAllLines(Paths.get("input/day12.txt"));
+        long startTime = System.currentTimeMillis();
+        processInput(input, 1);
+        System.out.println("Part I           6958 : " + countArrange() + "\nExecution time: " + (System.currentTimeMillis() - startTime) + "ms");
+        processInput(input, 5);
+        System.out.println("Part II 6555315065024 : " + countWithCache() + "\nExecution time: " + (System.currentTimeMillis() - startTime) + "ms");
+    }
+    
+    private static long countWithCache() {
         long res = 0;
-        for (int l = 0; l < len; l++) {
-            count = 0;
-            char[] puzz = getPuzzle(puzzle.get(l), wrap);
-            int[] cond = getCondition(condition.get(l), wrap);
-            helper("", puzz, cond, 0, 0, 0);
-            res += count;
+        for (int l = 0; l < puzzles.size(); l++) {
+            res += getArrangeWithCache(puzzles.get(l), conditions.get(l));
         }
         return res;
     }
     
-    // i -> index of current letter
-    // currCount -> consecutive # count so far
-    // need -> index of needed spring on array cond
-    private static void helper(String test, char[] puzz, int[] cond, int i, int currCount, int need) {
-        if (i == puzz.length) {
-            if (need == cond.length && currCount == 0 || need == cond.length - 1 && currCount == cond[need]) {
+    private static final Map<String, Long> cache = new HashMap<>();
+    
+    private static long getArrangeWithCache(String puzzle, List<Integer> condition) {
+        if (condition.isEmpty()) {
+            return puzzle.contains("#") ? 0 : 1;
+        }
+        if (puzzle.isEmpty()) {
+            return 0;
+        }
+        
+        String key = puzzle + condition;
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        
+        long res = 0;
+        char ch = puzzle.charAt(0);
+        int req = condition.get(0);
+        if (ch == '.' || ch == '?') {
+            res += getArrangeWithCache(puzzle.substring(1), condition);
+        }
+        if (ch == '#' || ch == '?') { // check: 1) all #, and 2) no more chars or next one is not #
+            if (req <= puzzle.length() && !puzzle.substring(0, req).contains(".")) {
+                if (req == puzzle.length() || puzzle.charAt(req) != '#') {
+                    res += getArrangeWithCache(puzzle.substring(Math.min(req + 1, puzzle.length())), condition.subList(1, condition.size()));
+                }
+            }
+        }
+        cache.put(key, res);
+        return res;
+    }
+    
+    // only works for part 1 but faster
+    private static long countArrange() {
+        for (int l = 0; l < puzzles.size(); l++) {
+            countArrange(puzzles.get(l), conditions.get(l), 0, 0, 0);
+        }
+        return count;
+    }
+    
+    private static void countArrange(String puzz, List<Integer> cond, int i, int currCount, int need) {
+        if (i == puzz.length()) {
+            if (need == cond.size() && currCount == 0 || need == cond.size() - 1 && currCount == cond.get(need)) {
                 count++;
             }
             return;
         }
-        if (puzz[i] == '?') {
+        if (puzz.charAt(i) == '?') {
             if (currCount > 0) {
-                if (need < cond.length && currCount == cond[need]) {
-                    helper(test + ".", puzz, cond, i + 1, 0, need + 1);
+                if (need < cond.size() && currCount == cond.get(need)) {
+                    countArrange(puzz, cond, i + 1, 0, need + 1);
                 }
             } else {
-                helper(test + ".", puzz, cond, i + 1, 0, need);
+                countArrange(puzz, cond, i + 1, 0, need);
             }
-            helper(test + "#", puzz, cond, i + 1, currCount + 1, need);
-        } else if (puzz[i] == '#') {
-            helper(test + "#", puzz, cond, i + 1, currCount + 1, need);
+            countArrange(puzz, cond, i + 1, currCount + 1, need);
+        } else if (puzz.charAt(i) == '#') {
+            countArrange(puzz, cond, i + 1, currCount + 1, need);
         } else {
             if (currCount > 0) {
-                if (need < cond.length && currCount != cond[need]) {
+                if (need < cond.size() && currCount != cond.get(need)) {
                     return;
                 } else {
                     need++;
                 }
             }
-            helper(test + ".", puzz, cond, i + 1, 0, need);
+            countArrange(puzz, cond, i + 1, 0, need);
         }
     }
     
-    private static char[] getPuzzle(char[] puzz, int wrap) {
-        List<Character> newPuzz = new ArrayList<>();
-        for (int c = 0; c < wrap; c++) {
-            for (char value : puzz) {
-                newPuzz.add(value);
-            }
-            if (wrap > 1) {
-                newPuzz.add('?');
-            }
+    private static void processInput(List<String> input, int wrap) {
+        puzzles = new ArrayList<>();
+        conditions = new ArrayList<>();
+        for (String line : input) {
+            String[] parts = line.split(" ");
+            puzzles.add(String.join("?", Collections.nCopies(wrap, parts[0])));
+            conditions.add(Stream.of(String.join(",", Collections.nCopies(wrap, parts[1])).split(",")).map(Integer::parseInt).collect(Collectors.toList()));
         }
-        puzz = new char[newPuzz.size()];
-        for (int i = 0; i < puzz.length; i++) {
-            puzz[i] = newPuzz.get(i);
-        }
-        return puzz;
-    }
-    
-    private static int[] getCondition(int[] cond, int wrap) {
-        List<Integer> newCond = new ArrayList<>();
-        for (int i = 0; i < cond.length * wrap; i++) {
-            newCond.add(cond[i % cond.length]);
-        }
-        cond = new int[newCond.size()];
-        for (int i = 0; i < cond.length; i++) {
-            cond[i] = newCond.get(i);
-        }
-        return cond;
-    }
-    
-    private static void processInput(Scanner in) {
-        puzzle = new ArrayList<>();
-        condition = new ArrayList<>();
-        while (in.hasNextLine()) {
-            String line = in.nextLine();
-            puzzle.add(line.split(" ")[0].toCharArray());
-            String[] cond = line.split(" ")[1].split(",");
-            int[] digits = new int[cond.length];
-            for (int i = 0; i < cond.length; i++) {
-                digits[i] = Integer.parseInt(cond[i]);
-            }
-            condition.add(digits);
-        }
-        len = puzzle.size();
     }
 }

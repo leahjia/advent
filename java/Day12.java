f27cd01f4b2e3bc5b3d7d65a5cc9ca73e8e33941
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-12-18 22:13:14 -0800

    nope

--- java/Day12.java
+++ java/Day12.java
@@ -10,112 +10,85 @@ public class Day12 {
         long startTime = System.currentTimeMillis();
         Scanner in = new Scanner(new FileReader("input/day12.txt"));
         processInput(in);
-        System.out.println("Part I  6958 : " + part1());
-        //System.out.println("Part II ?? : " + part2());
+        // System.out.println("Part I  6958 : " + countArrange(1));
+        System.out.println("Part II ?? : " + countArrange(5));
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
     
     static long count = 0;
     
-    private static long part1() {
+    private static long countArrange(int wrap) {
         long res = 0;
         for (int l = 0; l < len; l++) {
             count = 0;
-            int need = 0;
-            for (int n : condition.get(l)) {
-                need += n;
-            }
-            helper(puzzle.get(l), condition.get(l), 0, "", need);
+            char[] puzz = getPuzzle(puzzle.get(l), wrap);
+            int[] cond = getCondition(condition.get(l), wrap);
+            helper("", puzz, cond, 0, 0, 0);
             res += count;
         }
         return res;
     }
     
-    private static long part2() {
-        long res = 0;
-        for (int l = 0; l < len; l++) {
-            count = 0;
-            
-            char[] puzz = puzzle.get(l);
-            List<Character> newPuzz = new ArrayList<>();
-            for (int c = 0; c < 5; c++) {
-                for (int i = 0; i < puzz.length; i++) {
-                    newPuzz.add(puzz[i]);
-                }
-                newPuzz.add('?');
-            }
-            puzz = new char[newPuzz.size()];
-            for (int i = 0; i < puzz.length; i++) {
-                puzz[i] = newPuzz.get(i);
-            }
-            
-            int[] cond = condition.get(l);
-            List<Integer> newCond = new ArrayList<>();
-            int need = 0;
-            for (int i = 0; i < cond.length * 5; i++) {
-                newCond.add(cond[i % cond.length]);
-                need += cond[i % cond.length];
-            }
-            cond = new int[newCond.size()];
-            for (int i = 0; i < cond.length; i++) {
-                cond[i] = newCond.get(i);
-            }
-            
-            helper(puzz, cond, 0, "", need);
-            System.out.println(Arrays.toString(puzz) + " " + Arrays.toString(cond) + " - " + count + " arrangement(s)");
-            res += count;
-        }
-        return res;
-    }
-    
-    private static void helper(char[] puzz, int[] cond, int i, String str, int need) {
-        // System.out.println("   WHAT " + str + " and " + Arrays.toString(cond));
-        if (need < 0) {
-            return;
-        }
+    // i -> index of current letter
+    // currCount -> consecutive # count so far
+    // need -> index of needed spring on array cond
+    private static void helper(String test, char[] puzz, int[] cond, int i, int currCount, int need) {
         if (i == puzz.length) {
-            if (isPossible(str, cond)) {
-                // System.out.println("       Checked " + str + " and " + Arrays.toString(cond));
+            if (need == cond.length && currCount == 0 || need == cond.length - 1 && currCount == cond[need]) {
                 count++;
             }
-        } else if (puzz[i] == '?') {
-            helper(puzz, cond, i + 1, str + "#", need - 1);
-            helper(puzz, cond, i + 1, str + ".", need);
-        } else if (puzz[i] == '.') {
-            helper(puzz, cond, i + 1, str + ".", need);
+            return;
+        }
+        if (puzz[i] == '?') {
+            if (currCount > 0) {
+                if (need < cond.length && currCount == cond[need]) {
+                    helper(test + ".", puzz, cond, i + 1, 0, need + 1);
+                }
+            } else {
+                helper(test + ".", puzz, cond, i + 1, 0, need);
+            }
+            helper(test + "#", puzz, cond, i + 1, currCount + 1, need);
+        } else if (puzz[i] == '#') {
+            helper(test + "#", puzz, cond, i + 1, currCount + 1, need);
         } else {
-            helper(puzz, cond, i + 1, str + "#", need - 1);
+            if (currCount > 0) {
+                if (need < cond.length && currCount != cond[need]) {
+                    return;
+                } else {
+                    need++;
+                }
+            }
+            helper(test + ".", puzz, cond, i + 1, 0, need);
         }
     }
     
-    private static boolean isPossible(String str, int[] cond) {
-        char[] chars = str.toCharArray();
-        int i = 0;
-        int r = 0;
-        int curr = 0;
-        while (i < str.length()) {
-            char ch = chars[i];
-            if (ch == '#') {
-                curr++;
-                i++;
-            } else if (ch == '.') {
-                if (curr > 0) {
-                    if (r < cond.length && cond[r] == curr) {
-                        r++;
-                    } else {
-                        return false;
-                    }
-                }
-                curr = 0;
-                while (i < str.length() && chars[i] == '.') {
-                    i++;
-                }
+    private static char[] getPuzzle(char[] puzz, int wrap) {
+        List<Character> newPuzz = new ArrayList<>();
+        for (int c = 0; c < wrap; c++) {
+            for (char value : puzz) {
+                newPuzz.add(value);
+            }
+            if (wrap > 1) {
+                newPuzz.add('?');
             }
         }
-        if (curr > 0) {
-            return r == cond.length - 1 && cond[r] == curr;
+        puzz = new char[newPuzz.size()];
+        for (int i = 0; i < puzz.length; i++) {
+            puzz[i] = newPuzz.get(i);
         }
-        return r == cond.length;
+        return puzz;
+    }
+    
+    private static int[] getCondition(int[] cond, int wrap) {
+        List<Integer> newCond = new ArrayList<>();
+        for (int i = 0; i < cond.length * wrap; i++) {
+            newCond.add(cond[i % cond.length]);
+        }
+        cond = new int[newCond.size()];
+        for (int i = 0; i < cond.length; i++) {
+            cond[i] = newCond.get(i);
+        }
+        return cond;
     }
     
     private static void processInput(Scanner in) {

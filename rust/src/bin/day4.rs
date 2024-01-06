41c01091a9e76a29e53ac0893a54d545231fea39
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-05 21:50:04 -0800

    clear

--- rust/src/bin/day4.rs
+++ rust/src/bin/day4.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 
 lazy_static!(
     static ref FILE: String = std::fs::read_to_string("../input/day4.txt").unwrap();
-);
+    );
 
 fn main() {
     let (decks, winning_cards) = parse_input(&FILE);
@@ -16,23 +16,20 @@ fn parse_input(input: &str) -> (Vec<Vec<i32>>, Vec<HashSet<i32>>) {
     let mut decks: Vec<Vec<i32>> = Vec::new();
 
     for line in input.lines() {
-        let all_cards: Vec<_> = line.split(": ").nth(1).unwrap().split(" | ").map(|card| card.to_string()).collect();
-        winning_cards.push(all_cards.first().unwrap().split_whitespace().map(|card| card.parse::<i32>().unwrap()).collect());
-        decks.push(all_cards.get(1).unwrap().split_whitespace().map(|card| card.parse::<i32>().unwrap()).collect());
+        let all_cards: Vec<_> = line.splitn(2, ": ").nth(1).unwrap().split(" | ").collect();
+        winning_cards.push(all_cards[0].split_whitespace().map(|card| card.parse::<i32>().unwrap()).collect());
+        decks.push(all_cards[1].split_whitespace().map(|card| card.parse::<i32>().unwrap()).collect());
     }
     (decks, winning_cards)
 }
 
-fn part1(decks: &Vec<Vec<i32>>, winning_cards: &Vec<HashSet<i32>>) -> i32 {
+fn part1(decks: &[Vec<i32>], winning_cards: &[HashSet<i32>]) -> i32 {
     let mut res = 0;
-    for i in 0..decks.len() {
+    for (i, deck) in decks.iter().enumerate() {
         let mut ct = 0;
-        for card in decks[i].iter() {
+        for card in deck {
             if winning_cards[i].contains(card) {
-                ct *= 2;
-                if ct == 0 {
-                    ct = 1;
-                }
+                ct = if ct == 0 { 1 } else { ct * 2 };
             }
         }
         res += ct;
@@ -40,20 +37,19 @@ fn part1(decks: &Vec<Vec<i32>>, winning_cards: &Vec<HashSet<i32>>) -> i32 {
     res
 }
 
-fn part2(decks: &Vec<Vec<i32>>, winning_cards: &Vec<HashSet<i32>>) -> i32 {
+fn part2(decks: &[Vec<i32>], winning_cards: &[HashSet<i32>]) -> i32 {
     let mut matches = HashMap::new();
-    for i in 0..decks.len() {
-        for card in decks[i].iter() {
+    for (i, deck) in decks.iter().enumerate() {
+        for card in deck {
             if winning_cards[i].contains(card) {
-                matches.insert(i + 1, matches.get(&(i + 1)).unwrap_or(&0) + 1);
+                *matches.entry(i + 1).or_insert(0) += 1;
             }
         }
     }
 
     let mut res = decks.len() as i32;
     let mut q: VecDeque<_> = matches.keys().cloned().collect();
-    while !q.is_empty() {
-        let curr = q.pop_front().unwrap();
+    while let Some(curr) = q.pop_front() {
         for i in 1..=*matches.get(&curr).unwrap() {
             if matches.contains_key(&(curr + i)) {
                 q.push_back(curr + i);
@@ -61,7 +57,6 @@ fn part2(decks: &Vec<Vec<i32>>, winning_cards: &Vec<HashSet<i32>>) -> i32 {
             res += 1;
         }
     }
-
     res
 }
 

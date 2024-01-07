a3c2fe4c53343fdd0a4b2c088438758cd4726a77
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-06 22:26:02 -0800

    d5-2

--- rust/src/bin/day5.rs
+++ rust/src/bin/day5.rs
@@ -9,7 +9,7 @@ lazy_static!(
 fn main() {
     let (map_names, seeds, maps) = parse_input(&FILE);
     println!("Part I: Expected 424490994, Result {}", part1(&map_names, &seeds, &maps));
-    // println!("Part II: Expected 15290096, Result {}", part2(&map_names, &seeds, &maps));
+    println!("Part II: Expected 15290096, Result {}", part2(&map_names, &seeds, &maps));
 }
 
 fn part1(map_names: &[&str], seeds: &[i64], maps: &HashMap<&str, Vec<[i64; 3]>>) -> i64 {
@@ -33,6 +33,38 @@ fn get_map_to(seed: i64, ranges: &Vec<[i64; 3]>) -> i64 {
     seed
 }
 
+fn part2(map_names: &[&str], seeds: &[i64], maps: &HashMap<&str, Vec<[i64; 3]>>) -> i64 {
+    let mut mapped: Vec<[i64; 2]> = Vec::new();
+    for i in (0..seeds.len() - 1).step_by(2) {
+        mapped.push([seeds[i], seeds[i] + seeds[i + 1] - 1]);
+    }
+    for name in map_names {
+        mapped = get_map_to_range(maps.get(name).unwrap(), &mapped);
+    }
+
+    let mut lowest = i64::MAX;
+    for range in mapped {
+        lowest = lowest.min(range[0]);
+    }
+    lowest
+}
+
+fn get_map_to_range(map: &Vec<[i64; 3]>, ranges: &Vec<[i64; 2]>) -> Vec<[i64; 2]> {
+    let mut mapped = Vec::new();
+    for range in ranges {
+        for map_range in map {
+            let off_by = map_range[2];
+            if has_overlap(*range, *map_range) {
+                mapped.push([range[0].max(map_range[0]) + off_by, range[1].min(map_range[1]) + off_by]);
+            }
+        }
+    }
+    mapped
+}
+
+fn has_overlap(range1: [i64; 2], range2: [i64; 3]) -> bool {
+    !(range1[1] < range2[0] || range2[1] < range1[0])
+}
 
 fn parse_input(input: &str) -> (Vec<&str>, Vec<i64>, HashMap<&str, Vec<[i64; 3]>>) {
     let map_names = ["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"];
